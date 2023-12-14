541aa7e6ae930df8116a91c8176ec3c97a870829
Irina Vikhrova <ivikhrova@optimalgroup.ru>
2023-12-14 18:26:46 +0300

    add rest anumation

--- src/js/anim.js
+++ src/js/anim.js
@@ -16,7 +16,7 @@ gsap.to(".mount-layer", {
     end: "center top",
     scrub: true,
   },
-  y: -200,
+  y: -150,
 });
 
 gsap.to(".bg-layer", {
@@ -35,27 +35,99 @@ gsap.from(".mounts-intro", {
   scrollTrigger: {
     trigger: ".mounts",
     start: "5% 90%",
-    end: "15% 90%",
-    scrub: true,
+    end: "30% 90%",
+    scrub: 1,
+    // markers: true
   },
   y: 100,
   opacity: 0,
 });
 
 
-
 let sections = gsap.utils.toArray(".slide");
 
 let scrollTween = gsap.to(sections, {
-  xPercent: -112 * (sections.length - 1),
+  xPercent: -108 * (sections.length - 1),
   ease: "none", // <-- IMPORTANT!
   scrollTrigger: {
     trigger: "#slides",
     pin: false,
     scrub: 0.1,
-    //snap: directionalSnap(1 / (sections.length - 1)),
-    start: "top center",
-    end: "center center",
-    markers: true,
+    // snap: directionalSnap(1 / (sections.length - 1)),
+    start: "40% center",
+    end: "bottom center",
   }
 });
+
+gsap.to(".mounts__wrapper", {
+  scrollTrigger: {
+    trigger: ".mounts",
+    start: "90% 60%",
+    end: "bottom 60%",
+    scrub: 1,
+  },
+  y: -100,
+  opacity: 0,
+});
+
+gsap.from(".guides", {
+  scrollTrigger: {
+    trigger: ".mounts",
+    start: "90% 60%",
+    end: "bottom 60%",
+    scrub: 1,
+  },
+  y: 300,
+  opacity: 0,
+});
+
+
+gsap.from(".guides__list-item", {
+  scrollTrigger: {
+    trigger: ".guides__list",
+    start: "top center",
+    end: "bottom center",
+    scrub: 1,
+    // markers: true
+  },
+  y: 100,
+  opacity: 0,
+  stagger: 0.5,
+});
+
+gsap.to(".guides__wrapper", {
+  scrollTrigger: {
+    trigger: ".guides",
+    start: "90% 60%",
+    end: "bottom 60%",
+    scrub: 1,
+  },
+  y: -100,
+  opacity: 0,
+});
+
+gsap.from(".pricing__wrapper", {
+  scrollTrigger: {
+    trigger: ".guides",
+    start: "90% 60%",
+    end: "bottom 60%",
+    scrub: 1,
+  },
+  y: 300,
+  opacity: 0,
+});
+
+
+gsap.from(".pricing__card", {
+  scrollTrigger: {
+    trigger: ".pricing",
+    start: "20% center",
+    end: "70% center",
+    scrub: 1,
+  },
+  opacity: 0,
+  scale: 0.7,
+  stagger: 0.2,
+  ease: "elastic"
+},
+);
